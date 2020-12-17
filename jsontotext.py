924a972b528c047243a9928043a66d0dfd7befdb
Anupamdatta10 <59272422+Anupamdatta10@users.noreply.github.com>
2020-12-17 18:21:36 +0530

    utf-8 encoded

--- jsontotext.py
+++ jsontotext.py
@@ -1,11 +1,23 @@
 import json
 import requests
+import unidecode
 responce=requests.get("https://api.github.com/repos/laravel/laravel/stargazers?per_page=100&page=2")
 data=responce.json()
 
-f = open("demofile2.txt", "a")
+f = open("demofile2.txt", "a",encoding="utf-8")
+
 for i in data:
-    print(i["login"])
-    f.write(i["login"]+"\n")
+    print(str(i["login"]))
+    url="https://api.github.com/users/"+i["login"]
+    name=requests.get(url)
+    fullname=name.json()
+    n=str(fullname["name"])
+    firstname=n.split()
+    if len(firstname)==1:
+        f.write(str(i["login"])+", firstname="+firstname[0]+", lastname=none"+"\n")
+    else:
+        f.write(str(i["login"])+", firstname="+firstname[0]+", lastname="+firstname[-1]+"\n")
+    
+
 f.close()
 
